3969d18b9f8d326e4ed3144530b5e6ad741dad47
Desii99 <52660829+Desii99@users.noreply.github.com>
2020-05-18 01:28:11 +0200

    Add files via upload

--- src/PaymentData.py
+++ src/PaymentData.py
@@ -1,4 +1,23 @@
 class PaymentData:
 
-    def __init__(self):
+    def __init__(self,tarj_VISA,tarj_MASTERCARD,num_tarj,cod_seg,importe):
+        self.tarj_VISA = 'VISA'
+        self.tarj_MASTERCARD = 'MASTERCARD'
+        self.num_tarj = num_tarj
+        self.cod_seg = cod_seg
+        self.importe = importe
         pass
+
+    def Error_pago(self,tarj_MASTERCARD,tarj_VISA,num_tarj,cod_seg,importe):
+        error = False
+        if tarj_MASTERCARD == 'MASTERCARD' and tarj_VISA == 'VISA':
+            error = True
+        if tarj_VISA != 'VISA' and tarj_MASTERCARD != 'MASTERCARD':
+            error = True
+        if num_tarj == '':
+            error = True
+        if cod_seg == '':
+            error = True
+        return error
+    pass
+        

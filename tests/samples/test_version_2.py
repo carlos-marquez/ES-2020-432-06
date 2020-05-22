c7c767eefa3fe95e0477136d0a31f3acfeda3cd6
PolCarulla <pol.carulla.sanchez@gmail.com>
2020-05-23 00:07:29 +0200

    test 2 version 2 terminado

--- tests/samples/test_version_2.py
+++ tests/samples/test_version_2.py
@@ -26,7 +26,20 @@ class Test_v2:
 
         assert(x.payment_data.tipo_tarjeta == 'MASTERCARD')
 
-   
+    def test2(self):
+        aux1 = Vuelos(destino = 'BCN')
+        aux2 = Vuelos(destino = 'ITA')
+        aux_vuelo = [aux1, aux2]
+
+        y = PaymentData('MASTERCARD', 'Pepito', '4546')
+        x = Viajes(user = User, lista_pasajeros = ['p1', 'p2', 'p3'], vuelos = aux_vuelo)
+        aux = User('Pol', '12345678J', '08390', '123456789', 'user@uab.cat')
+
+        fallo = x.payment_V2(y, 1)
+        Bank.do_payment = MagicMock(return_value = False)
+        i = Bank()
+
+        assert i.do_payment(aux, y) == fallo
 
 
 Test_v2.test1(1)

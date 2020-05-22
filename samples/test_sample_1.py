5e0ca070b1ac88483f5ddcdd9b3f5596bab1a257
Desii99 <desireepasion.rodriguez@e-campus.uab.cat>
2020-05-22 14:47:00 +0200

    commit

--- samples/test_sample_1.py
+++
@@ -1,8 +0,0 @@
-class TestCars:
-    def test_one(self):
-        x = "this"
-        assert "h" in x
-
-    def test_two(self):
-        x = 3+3
-        assert x == 6
--- test_1.py
+++ test_1.py
@@ -8,14 +8,20 @@ from src.Bank import Bank
 from src.PaymentData import PaymentData
 sys.path.append(os.path.realpath('../../src'))
 class test_1:
-    def test_error_pagament:
-        '''
-        Dado un viaje con múltiples destinos y más 
-        de un viajero, cuando se produce un error al 
-        realizar el pago, se reporta que la acción 
-        no se ha podido realizar
-        ''' 
-        error = PaymentData('VISA','',87296785,352,582)
-        resultado_esperado = Error_pago('VISA','',87296785,352,582)
-        assert resultado_esperado == True
-        print('La acción no se ha realizado correctamente. Pago incorrecto')
+    #def test_error_pagament:
+        #'''
+        #Dado un viaje con múltiples destinos y más 
+        #de un viajero, cuando se produce un error al 
+        #realizar el pago, se reporta que la acción 
+        #no se ha podido realizar
+        #''' 
+        #error = PaymentData('VISA','',87296785,352,582)
+        #resultado_esperado = Error_pago('VISA','',87296785,352,582)
+        #assert resultado_esperado == True
+        #print('La acción no se ha realizado correctamente. Pago incorrecto')
+    def test_facturacion_correcta:
+        '''Dado un viaje con múltiples destinos y más de un viajero, cuando los datos de
+        facturación introducidos por el usuario son correctos, se reporta que los datos
+        son correctos'''
+
+        

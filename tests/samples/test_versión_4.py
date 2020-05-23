6ac4d87767b30f48f4165b9ec17a60b3572f2a04
Danielibus <dani.villanueva@e-campus.uab.cat>
2020-05-23 12:25:51 +0200

    cmmit 1

--- tests/samples/test_versión_4.py
+++ tests/samples/test_versión_4.py
@@ -12,10 +12,43 @@ from unittest.mock import MagicMock
 from src.Bank import Bank
 from src.User import User
 
+class Test_v4:
+#reintentar pago cuando hay error
+    def test_1(self):
+        destinos = ['BCN', 'NYC']
+        viajeros = 2
+        
+#reintentar y pago correcto 
+    def test_2(self):
+        destinos = ['BCN', 'NYC', 'LA']
+        viajeros = 2
+#numero maximo de reintentos realizar pago, no se puede realizar accion
+    def test_3(self):
+        destinos = ['BCN', 'NYC', 'LA']
+        viajeros = 2 
+#error confirmar vuelos, reintenta confirmacion    
+    def test_4(self):
+        destinos = ['BCN', 'NYC', 'LA']
+        viajeros = 2 
+#confirmación ed vuelos correcta en un reintento
+    def test_5(self):
+        destinos = ['BCN', 'NYC', 'LA']
+        viajeros = 2
+#numero maximo de reintentos confirmar vuelo
+    def test_6(self):
+        destinos = ['BCN', 'NYC', 'LA']
+        viajeros = 2
 
-def tets_4_1(self):
-    destinos = ['dest1', 'dest2']
-    viajeros = 2
+
+
+
+
+
+    
+
+
+
+
     
 
 
